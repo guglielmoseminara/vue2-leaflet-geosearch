a55703ff901de5c6a3b4314f54c33aa9979ea301
gully <guglielmo.seminara@gmail.com>
2019-07-19 14:03:36 +0200

    feat(setquery): feat set query

--- Vue2LeafletGeosearch.vue
+++ Vue2LeafletGeosearch.vue
@@ -22,21 +22,30 @@ export default {
   beforeDestroy() {
     this.remove();
   },
+  data(){
+    return {
+      query: undefined,
+      searchControl: undefined
+    }
+  },
   methods: {
     deferredMountedTo(parent) {
-      const searchControl = new GeoSearchControl(this.options);
-      parent.addControl(searchControl);
+      this.searchControl = new GeoSearchControl(this.options);
+      parent.addControl(this.searchControl);
     },
     remove() {
       if (this.markerCluster) {
         this.$parent.removeLayer(this.markerCluster);
-      }      
+      }
     },
     add() {
       if (this.$parent._isMounted) {
         this.deferredMountedTo(this.$parent.mapObject);
       }
     },
+    setQuery(query) {
+      this.searchControl.searchElement.elements.input.value = query;
+    }
   },
 };
 </script>
